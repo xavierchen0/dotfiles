12c18b01bc182702d3a67b833b347779338a4c5d
Xavier Chen <xaviernehc@gmail.com>
2024-09-15 21:45:17 +0800

    Add the vim-flog plugin to have git graph functionality

---
+++ nvim/lua/plugins/gitgraph.lua
@@ -0,0 +1,11 @@
+-- Add git graph functionality to NeoVim
+return {
+  {
+    "rbong/vim-flog",
+    lazy = true,
+    cmd = { "Flog", "Flogsplit", "Floggit" },
+    dependencies = {
+      "tpope/vim-fugitive",
+    },
+  },
+}
