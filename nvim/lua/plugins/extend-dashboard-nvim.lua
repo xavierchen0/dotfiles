57836046d859bd15267404e7551960790aea0871
Xavier Chen <xaviernehc@gmail.com>
2024-09-15 16:41:00 +0800

    Changed dashboard logo
    Added another lua file in nvim/lua/plugins to modify the default options
    of dashboard-nvim

---
+++ nvim/lua/plugins/extend-dashboard-nvim.lua
@@ -0,0 +1,24 @@
+-- Change dashboard logo
+return {
+  {
+    "nvimdev/dashboard-nvim",
+
+    -- Pass default options
+    opts = function(_, opts)
+      -- Modify logo
+      local logo = [[
+                                                          
+        ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+        ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+        ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+        ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+        ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+        ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+                                                          
+      ]]
+
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+    end,
+  },
+}
