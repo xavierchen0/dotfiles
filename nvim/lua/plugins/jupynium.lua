eb8fd48c45a18f6594eae21dae468dd3e6bb4211
Xavier Chen <xaviernehc@gmail.com>
2024-09-15 21:43:07 +0800

    Fix Jupynium and nvim-cmp modification that was throwing errors
    The error previously was that Jupynium plugin was not loaded in first
    which prevents the use of Jupynium.

--- nvim/lua/plugins/jupynium.lua
+++ nvim/lua/plugins/jupynium.lua
@@ -1,30 +1,6 @@
 return {
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "kiyoon/jupynium.nvim",
-      build = "pip3 install --user .",
-    },
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      local compare = cmp.config.compare
-
-      local custom_opts = {
-        sources = {
-          { name = "jupynium", priority = 1000 },
-          { name = "nvim_lsp", priority = 100 },
-        },
-        sorting = {
-          priority_weight = 1.0,
-          comparators = {
-            compare.score,
-            compare.recently_used,
-            compare.locality,
-          },
-        },
-      }
-
-      opts = vim.tbl_deep_extend("force", opts, custom_opts)
-    end,
+    "kiyoon/jupynium.nvim",
+    build = "pip3 install --user .",
   },
 }
