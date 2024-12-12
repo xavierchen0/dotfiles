7639591c6f3a3ee80628203ccb1ee0bdd94e2182
Xavier Chen <xaviernehc@gmail.com>
2024-12-12 16:14:48 +0800

    Wezterm / Update: Moved terminals from iTerm2 to Wezterm; Added the Wezterm config file

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,39 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration
+local config = wezterm.config_builder()
+
+-- #######################
+--
+-- MY CONFIG GOES HERE
+--
+-- #######################
+
+-- Colorscheme
+config.color_scheme = "rose-pine"
+
+-- Font
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 20.0
+-- disable ligatures
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+
+-- Keyboard
+-- Below is to disable dead key processing so that ^ can be used with a single keypress
+config.use_dead_keys = false
+
+-- Remove the macos bar at the top with the 3 buttons
+config.window_decorations = "RESIZE"
+
+-- Tabs
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Scrollback
+config.keys = {
+	{ key = "U", mods = "CTRL|SHIFT", action = wezterm.action.ScrollByPage(-0.5) },
+	{ key = "D", mods = "CTRL|SHIFT", action = wezterm.action.ScrollByPage(0.5) },
+}
+
+config.max_fps = 120
+return config
