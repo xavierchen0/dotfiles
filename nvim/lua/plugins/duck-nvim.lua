8949c26d4fe3af00d25b16b9d880bb58ee537bf2
Xavier Chen <xaviernehc@gmail.com>
2024-12-23 22:17:14 +0800

    Nvim / Update: Add duck.nvim plugin that spawns ducks

---
+++ nvim/lua/plugins/duck-nvim.lua
@@ -0,0 +1,18 @@
+return {
+  "tamton-aquib/duck.nvim",
+  -- e.g. change custom character: nnoremap <leader>dd :lua require("duck").hatch("ඞ")<CR>
+  -- e.g. change speed eg 1 vim.keymap.set('n', '<leader>dd', function() require("duck").hatch("🦆", 10) end, {}) -- A pretty fast duck
+  -- e.g. change speed eg 2 vim.keymap.set('n', '<leader>dc', function() require("duck").hatch("🐈", 0.75) end, {}) -- Quite a mellow cat
+
+  config = function()
+    vim.api.nvim_create_user_command("DuckHatch", function()
+      require("duck").hatch()
+    end, {})
+    vim.api.nvim_create_user_command("DuckCook", function()
+      require("duck").cook()
+    end, {})
+    vim.api.nvim_create_user_command("DuckCookAll", function()
+      require("duck").cook_all()
+    end, {})
+  end,
+}
