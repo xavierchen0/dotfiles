3cd789b4bf1dd74a22575d1184b4ecee8f45c3b4
Xavier Chen <xaviernehc@gmail.com>
2024-12-28 21:04:13 +0800

    Nvim / Update: Extended the default blink-cmp plugin with my own configuration.
    Included keymaps to jump forward and backward in snippets, even though
    by right, these keymaps should already be active

---
+++ nvim/lua/plugins/extend-blink-cmp.lua
@@ -0,0 +1,16 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "default",
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+      ["<Tab>"] = { "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+    },
+
+    signature = { enabled = true },
+
+    completion = { ghost_text = { enabled = false } },
+  },
+}
