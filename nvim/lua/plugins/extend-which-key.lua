3ac2b858082c9f88b0537415eaaa4adf7e3c5dc8
Xavier Chen <xaviernehc@gmail.com>
2025-01-12 15:47:37 +0000

    Nvim / Update: Installed Jupynium plugin for Jupyter support in Neovim, and installed jupytext plugin to autoconvert any .ipynb file to jupytext format

---
+++ nvim/lua/plugins/extend-which-key.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    spec = {
+      mode = { "n", "v", "x" },
+      { "<leader>j", group = "Jupyter" },
+    },
+  },
+}
---
+++ nvim/lua/plugins/jupytext.lua
@@ -0,0 +1,5 @@
+return {
+  "GCBallesteros/jupytext.nvim",
+  lazy = false,
+  config = true,
+}
