92162449c712c38a6ca7bb570b4f07330aac0654
Xavier Chen <xaviernehc@gmail.com>
2025-04-06 22:01:11 +0100

    fix(nvim): fix border size for LSP signature help popup
    When you press 'K' to get LSP signature help on a function object, it
    will create a very big popup window if there are many parameters in the
    function object and/or the documentation is long. The problem is caused
    by the noice plugin. Fixed this by reducing the default maxheight and
    maxwidth of the hover window.

---
+++ nvim/lua/plugins/extend-noice.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    views = {
+      hover = {
+        size = {
+          max_height = 10,
+          max_width = 50,
+        },
+      },
+    },
+  },
+}
