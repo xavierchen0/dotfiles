99f9d1d2e82237bd2f86c002e5308ddcc3d2d58f
Xavier Chen <xaviernehc@gmail.com>
2025-04-09 17:23:45 +0100

    feat(nvim): Add new Kanagawa colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
   { "rose-pine/neovim", name = "rose-pine" },
   { "ellisonleao/gruvbox.nvim", name = "gruvbox" },
+  { "rebelot/kanagawa.nvim", name = "kanagawa" },
   {
     "LazyVim/LazyVim",
     opts = {
