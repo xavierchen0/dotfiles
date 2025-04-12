0b8c3fde3b8019330aecafa8525abc121c16a75e
Xavier Chen <xaviernehc@gmail.com>
2025-04-12 22:15:45 +0100

    feat(nvim): add colors when I paste
    Link Yanky.nvim's YankyPut highlight group to IncSearch highlight group
    so I get colors when I paste.

--- nvim/lua/plugins/extend-yanky.lua
+++ nvim/lua/plugins/extend-yanky.lua
@@ -2,5 +2,6 @@ return {
   "gbprod/yanky.nvim",
   init = function(_)
     vim.api.nvim_set_hl(0, "YankyYanked", { link = "IncSearch" })
+    vim.api.nvim_set_hl(0, "YankyPut", { link = "IncSearch" })
   end,
 }
