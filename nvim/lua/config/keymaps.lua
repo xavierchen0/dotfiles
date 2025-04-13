924ec389b6765f1169bb6e796065c16c1a1c53bf
Xavier Chen <xaviernehc@gmail.com>
2025-04-13 22:11:26 +0100

    feat(nvim): add a new keymap to use native diagnostic virtual lines

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,15 @@
 -- Add any additional keymaps here
 vim.keymap.set("n", "<Leader>o", "o<Esc>", { desc = "Add new line below and stay in Normal mode" })
 vim.keymap.set("n", "<Leader>O", "O<Esc>", { desc = "Add new line above and stay in Normal mode" })
+
+vim.keymap.set("n", "<leader>uk", function()
+  vim.diagnostic.config({ virtual_lines = { current_line = true }, virtual_text = false })
+
+  vim.api.nvim_create_autocmd("CursorMoved", {
+    group = vim.api.nvim_create_augroup("line-diagnostics", { clear = true }),
+    callback = function()
+      vim.diagnostic.config({ virtual_lines = false, virtual_text = true })
+      return true
+    end,
+  })
+end)
