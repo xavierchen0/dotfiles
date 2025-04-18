52fd527e13394f399a11b0e9b1b6ebfca2b80c65
Xavier Chen <xaviernehc@gmail.com>
2025-04-18 22:19:21 +0100

    feat(nvim): add autocmds to show active linters in current buffer

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Show linters for the current buffer's file type
+vim.api.nvim_create_user_command("LintInfo", function()
+  local filetype = vim.bo.filetype
+  local linters = require("lint").linters_by_ft[filetype]
+
+  if linters then
+    print("Linters for " .. filetype .. ": " .. table.concat(linters, ", "))
+  else
+    print("No linters configured for filetype: " .. filetype)
+  end
+end, {})
