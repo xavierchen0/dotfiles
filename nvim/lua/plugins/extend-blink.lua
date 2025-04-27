151959a0fd7997d06ef26864381fcc827f80400b
Xavier Chen <xaviernehc@gmail.com>
2025-04-27 09:11:36 +0200

    feat(nvim): change blink keymap preset to supertab and use <C-j> and <C-k> to navigate completions

--- nvim/lua/plugins/extend-blink.lua
+++ nvim/lua/plugins/extend-blink.lua
@@ -2,7 +2,9 @@ return {
   "saghen/blink.cmp",
   opts = {
     keymap = {
-      preset = "default",
+      preset = "super-tab",
+      ["<C-j>"] = { "select_next", "fallback_to_mappings" },
+      ["<C-k>"] = { "select_prev", "fallback_to_mappings" },
     },
   },
 }
