742622c323ea3589c874f9366e179286bc2497cb
Xavier Chen <xaviernehc@gmail.com>
2025-08-17 13:48:51 +0800

    feat(nvim): remove flash plugin

--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -1,49 +1,10 @@
 return {
-  -- Better code navigation
-  'folke/flash.nvim',
-  event = 'VeryLazy',
-  opts = {},
-  keys = {
-    {
-      's',
-      mode = { 'n', 'x', 'o' },
-      function()
-        require('flash').jump()
-      end,
-      desc = 'Flash',
-    },
-    {
-      'S',
-      mode = { 'n', 'x', 'o' },
-      function()
-        require('flash').treesitter()
-      end,
-      desc = 'Flash Treesitter',
-    },
-    {
-      'r',
-      mode = 'o',
-      function()
-        require('flash').remote()
-      end,
-      desc = 'Remote Flash',
-    },
-    {
-      'R',
-      mode = { 'o', 'x' },
-      function()
-        require('flash').treesitter_search()
-      end,
-      desc = 'Treesitter Search',
-    },
-  },
-
-  -- Better around/inside text objects
-  -- Examples:
-  --  - va)  - [V]isually select [A]round [)]paren
-  --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-  --  - ci'  - [C]hange [I]nside [']quote
   {
+    -- Better around/inside text objects
+    -- Examples:
+    --  - va)  - [V]isually select [A]round [)]paren
+    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+    --  - ci'  - [C]hange [I]nside [']quote
     'echasnovski/mini.ai',
     version = '*',
     opts = function()
