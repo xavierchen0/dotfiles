cf93fdb448e325c7877ac3004a28dded0ea7c1b3
Xavier Chen <xaviernehc@gmail.com>
2025-10-08 15:02:23 +0800

    feat(zsh): add custom command for jupynium

--- zsh/omz/custom/cmds_aliases.zsh
+++ zsh/omz/custom/cmds_aliases.zsh
@@ -4,8 +4,12 @@ alias dev="cd $HOME/dev/"
 
 alias ls="eza --icons=always"
 
-cd () {
+function cd () {
   builtin cd "$1" && ls -a
 }
 
 alias lvim='NVIM_APPNAME=nvim-lazyvim nvim'
+
+function jup () {
+  uv run jupynium --firefox_profiles_ini_path "/Users/xavier/Library/Application Support/Firefox/profiles.ini" --firefox_profile_name jupyter --nvim_listen_addr "$1"
+}
