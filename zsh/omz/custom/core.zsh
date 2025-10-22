e4766d811694ea9f9b492092a62e8029993d61bf
Xavier Chen <xaviernehc@gmail.com>
2025-10-22 12:09:09 +0800

    feat(zsh): set default man page width

--- zsh/omz/custom/core.zsh
+++ zsh/omz/custom/core.zsh
@@ -3,6 +3,7 @@ export VISUAL="nvim"
 export XDG_CONFIG_HOME="$HOME/.config/"
 export NVIM_RPLUGIN_MANIFEST="/Users/xavier/.local/share/nvim/rplugin.vim"
 export MANPAGER='nvim +Man!'
+export MANWIDTH=100
 # Spark
 export JAVA_HOME=$(/usr/libexec/java_home -v 17)
 export SPARK_HOME="/opt/homebrew/Cellar/apache-spark/3.5.4/libexec/"
