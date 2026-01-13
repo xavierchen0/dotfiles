312dfd7ab5de29ac13ca4493a06998bf96a81374
Xavier Chen <xaviernehc@gmail.com>
2026-01-13 19:14:36 +0800

    nvim: add init

---
+++ nvim/init.lua
@@ -0,0 +1,16 @@
+-- Define config table to be able to pass data between scripts
+_G.Config = {}
+
+-- Define custom autocommand group and helper to create an autocommand.
+-- Autocommands are Neovim's way to define actions that are executed on events
+-- (like creating a buffer, setting an option, etc.).
+--
+-- See also:
+-- - `:h autocommand`
+-- - `:h nvim_create_augroup()`
+-- - `:h nvim_create_autocmd()`
+local gr = vim.api.nvim_create_augroup('custom-config', {})
+_G.Config.new_autocmd = function(event, pattern, callback, desc)
+  local opts = { group = gr, pattern = pattern, callback = callback, desc = desc }
+  vim.api.nvim_create_autocmd(event, opts)
+end
