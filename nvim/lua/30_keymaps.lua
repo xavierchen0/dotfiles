f28af019f014f10c2f97843b8ed5572158ec464e
Xavier Chen <xaviernehc@gmail.com>
2026-01-14 20:38:53 +0800

    nvim: add keymap to center after half-page scroll

--- nvim/lua/30_keymaps.lua
+++ nvim/lua/30_keymaps.lua
@@ -3,3 +3,8 @@ vim.cmd.cabbrev("h", "vert h")
 
 -- Remove search highlight on <ESC> press
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR><Esc>")
+
+-- Center every scroll
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down half page and center' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up half page and center' })
+
