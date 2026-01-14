8d6557f9062a9dc5d910a9c4f1ddc03c67c30481
Xavier Chen <xaviernehc@gmail.com>
2026-01-14 19:33:20 +0800

    nvim: add conform formatter

---
+++ nvim/plugin/03_conform.lua
@@ -0,0 +1,13 @@
+vim.pack.add({
+  { src = 'https://github.com/stevearc/conform.nvim', name = 'conform' }
+})
+
+require('conform').setup({
+  formatters_by_ft = {
+    lua = { 'styua' }
+  },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_format = 'fallback',
+  }
+})
