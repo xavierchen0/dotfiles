e780c5dbf87ba94e191892408d8689430bc33985
Xavier Chen <xaviernehc@gmail.com>
2026-01-14 11:30:29 +0800

    nvim: add mini.snippets plugin

---
+++ nvim/plugin/70_mini_snippet.lua
@@ -0,0 +1,13 @@
+vim.pack.add({ 'https://github.com/nvim-mini/mini.snippets' })
+
+local gen_loader = require('mini.snippets').gen_loader
+require('mini.snippets').setup({
+  snippets = {
+    -- Load custom file with global snippets first
+    gen_loader.from_file('~/.config/nvim/snippets/global.lua'),
+
+    -- Load snippets based on current language by reading files from
+    -- "snippets/" subdirectories from 'runtimepath' directories.
+    gen_loader.from_lang(),
+  },
+})
