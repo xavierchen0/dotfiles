cabba124c7ec856765c4d9b12ee9b229a5515174
Xavier Chen <xaviernehc@gmail.com>
2026-01-14 20:07:16 +0800

    nvim: add marksman lsp

--- nvim/plugin/00_lsp.lua
+++ nvim/plugin/00_lsp.lua
@@ -1,6 +1,6 @@
 vim.pack.add({
-	-- Provides default lsp configs
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
+  -- Provides default lsp configs
+  { src = "https://github.com/neovim/nvim-lspconfig" },
 })
 
-vim.lsp.enable({ "lua_ls" })
+vim.lsp.enable({ "lua_ls", "marksman" })
