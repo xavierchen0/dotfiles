4ffc30cd485cf1272e8da3fda27b0613d0125467
Xavier Chen <xaviernehc@gmail.com>
2026-01-14 20:39:09 +0800

    nvim: add keymap to move diagnostic to location list

--- nvim/lua/30_keymaps.lua
+++ nvim/lua/30_keymaps.lua
@@ -8,3 +8,6 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR><Esc>")
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down half page and center' })
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up half page and center' })
 
+-- Add diagnostic to location list
+vim.keymap.set('n', '<leader>ld', vim.diagnostic.setloclist)
+
