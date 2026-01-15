5c8b335f9192ab24b4de99d5ffd877f8e28c1601
Xavier Chen <xaviernehc@gmail.com>
2026-01-15 18:32:34 +0800

    nvim: add keymap to toggle diagnostic

--- nvim/lua/30_keymaps.lua
+++ nvim/lua/30_keymaps.lua
@@ -11,6 +11,11 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up half page and center
 -- Add diagnostic to location list
 vim.keymap.set("n", "<leader>ld", vim.diagnostic.setloclist)
 
+-- Toggle diagnostics
+vim.keymap.set('n', '<leader>d', function()
+  vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end, { desc = "Toggle diagnostics" })
+
 -- Better movement with j/k even with wrapped lines
 vim.keymap.set("n", "j", "gj", { desc = "Move down by visual line (not logical line)" })
 vim.keymap.set("n", "k", "gk", { desc = "Move up by visual line (not logical line)" })
