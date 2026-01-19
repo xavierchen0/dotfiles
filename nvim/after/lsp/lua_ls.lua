8066c63a43b75588a7f102e45fae069573e76245
Xavier Chen <xaviernehc@gmail.com>
2026-01-19 09:05:27 +0800

    nvim: update lua_ls to only use vim's runtime files instead of the entire config file
    I realise that lua_ls takes a long time to load which is quite annoying

--- nvim/after/lsp/lua_ls.lua
+++ nvim/after/lsp/lua_ls.lua
@@ -2,9 +2,14 @@ return {
   settings = {
     Lua = {
       workspace = {
-        library = vim.tbl_filter(function(d)
-          return not d:match(vim.fn.stdpath("config") .. "/?a?f?t?e?r?")
-        end, vim.api.nvim_get_runtime_file("", true)),
+        -- library = vim.tbl_filter(function(d)
+        --   return not d:match(vim.fn.stdpath("config") .. "/?a?f?t?e?r?")
+        -- end, vim.api.nvim_get_runtime_file("", true)),
+        library = {
+          vim.env.VIMRUNTIME,
+          "${3rd}/luv/library",
+          "${3rd}/busted/library",
+        },
       },
     },
   },
