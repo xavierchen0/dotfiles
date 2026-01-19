8390f91c9f863b04b45833b82f41c0509b5ffd47
Xavier Chen <xaviernehc@gmail.com>
2026-01-19 09:06:10 +0800

    nvim: remove DiagnosticUnnecessary highlight
    Hard to see when it becomes gray

--- nvim/lua/00_colorscheme.lua
+++ nvim/lua/00_colorscheme.lua
@@ -4,4 +4,11 @@ vim.pack.add({
 
 require("rose-pine").setup()
 
+-- Unlink DiagnosticUnnecessary so it does not gray out unused variables/functions
+-- Apply autocommand whenever we load/switch colorscheme
+local f = function()
+  vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { link = "NONE" })
+end
+_G.Config.new_autocmd("ColorScheme", nil, f, "Remove Unused highlight when colorscheme loads/switch")
+
 vim.cmd.colorscheme("rose-pine")
