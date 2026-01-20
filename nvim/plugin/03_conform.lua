25f2ad12c8b292d9a1f2f8d30611fb81d10a6ad6
Xavier Chen <xaviernehc@gmail.com>
2026-01-20 20:59:32 +0800

    nvim: add ruff formatter for python

--- nvim/plugin/03_conform.lua
+++ nvim/plugin/03_conform.lua
@@ -6,6 +6,14 @@ require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
     markdown = { "prettier" },
+    python = {
+      -- To fix auto-fixable lint errors.
+      "ruff_fix",
+      -- To run the Ruff formatter.
+      "ruff_format",
+      -- To organize the imports.
+      "ruff_organize_imports",
+    },
   },
   format_on_save = {
     timeout_ms = 500,
