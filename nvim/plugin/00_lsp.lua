49ca548b50df866c68f84d269241301ebafe535e
Xavier Chen <xaviernehc@gmail.com>
2026-01-20 21:00:01 +0800

    nvim: add keymap for code actions

--- nvim/plugin/00_lsp.lua
+++ nvim/plugin/00_lsp.lua
@@ -3,4 +3,9 @@ vim.pack.add({
   { src = "https://github.com/neovim/nvim-lspconfig" },
 })
 
+local f = function()
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+end
+_G.Config.new_autocmd("LspAttach", nil, f, "Code Actions")
+
 vim.lsp.enable({ "lua_ls", "marksman" })
