373cea9aa8328c0ebd86a1fe8f30d9a92179d105
Xavier Chen <xaviernehc@gmail.com>
2026-02-01 13:59:59 +0800

    yazi: add bookmarks plugin

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,1 +1,19 @@
 require("folder-rules"):setup()
+
+require("bookmarks"):setup({
+  last_directory = { enable = true, persist = true, mode = "jump" },
+  persist = "vim",
+  desc_format = "full",
+  file_pick_mode = "parent",
+  custom_desc_input = false,
+  show_keys = false,
+  notify = {
+    enable = false,
+    timeout = 1,
+    message = {
+      new = "New bookmark '<key>' -> '<folder>'",
+      delete = "Deleted bookmark in '<key>'",
+      delete_all = "Deleted all bookmarks",
+    },
+  },
+})
