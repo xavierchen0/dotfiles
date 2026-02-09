039135b7eb0c0bfe6aed57096754d5338b03783e
Xavier Chen <xaviernehc@gmail.com>
2026-02-10 02:33:13 +0800

    nvim: add and configure typst lsp

---
+++ nvim/after/lsp/tinymist.lua
@@ -0,0 +1,8 @@
+return {
+  settings = {
+    formatterMode = "typstyle",
+    formatterProseWrap = true, -- wrap lines in content mode
+    formatterPrintWidth = 80,
+    formatterIndentSize = 4,
+  },
+}
