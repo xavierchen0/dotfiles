ead798945e1324510cfd4dfc41a10101f67c8497
Xavier Chen <xaviernehc@gmail.com>
2026-03-13 20:28:50 +0800

    nvim(snippets): fixed wrong argument

--- nvim/snippets/python.lua
+++ nvim/snippets/python.lua
@@ -1,7 +1,7 @@
 return {
   marimo_cells = {
     prefix = "'cc",
-    body = "@app.cell(${1|,hidden_code=True|})\ndef ${2:_}():\n$0",
+    body = "@app.cell(${1|,hide_code=True|})\ndef ${2:_}():\n$0",
     desc = "Create marimo app cells with choice of hidden_code",
   },
 }
