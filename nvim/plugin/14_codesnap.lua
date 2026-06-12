449f9bb55907a3ac3fea3931f122b9f54f252d68
Xavier Chen <xaviernehc@gmail.com>
2026-06-12 19:51:25 +0800

    nvim(codesnap): customise codesnap image

--- nvim/plugin/14_codesnap.lua
+++ nvim/plugin/14_codesnap.lua
@@ -1,3 +1,14 @@
 vim.defer_fn(function()
   vim.pack.add({ "https://github.com/mistricky/codesnap.nvim" })
+
+  require("codesnap").setup({
+    snapshot_config = {
+      window = {
+        margin = {
+          x = 10,
+          y = 10,
+        },
+      },
+    },
+  })
 end, 50)
